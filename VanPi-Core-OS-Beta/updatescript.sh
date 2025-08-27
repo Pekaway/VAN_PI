601186fe667a502806c31b25f19e0cef5eb917da
vincePekaway <77489211+vincePekaway@users.noreply.github.com>
2025-08-27 14:17:32 +0200

    b2.0.8

--- VanPi-Core-OS-Beta/updatescript.sh
+++ VanPi-Core-OS-Beta/updatescript.sh
@@ -11,10 +11,10 @@
 ########################################################
 
 # define variables
-Server='https://raw.githubusercontent.com/Pekaway/VAN_PI/main/VanPi-Core-OS/'
-ServerFiles='https://github.com/Pekaway/VAN_PI/raw/main/VanPi-Core-OS/'
-LfsServerFiles='https://media.githubusercontent.com/media/Pekaway/VAN_PI/main/VanPi-Core-OS/'
-Version='v2.0.8'		### <--- set new version number VanPi OS
+Server='https://raw.githubusercontent.com/Pekaway/VAN_PI/main/VanPi-Core-OS-Beta/'
+ServerFiles='https://github.com/Pekaway/VAN_PI/raw/main/VanPi-Core-OS-Beta/'
+LfsServerFiles='https://media.githubusercontent.com/media/Pekaway/VAN_PI/main/VanPi-Core-OS-Beta/'
+Version='b2.0.8'		### <--- set new version number VanPi OS
 NSPanelVersion='0.0.1'	### <--- set new version number NSPanel
 TouchdisplayVersion='2.0.4'	### <--- set new version number Touchdisplay
 currentVersion=`cat ~/pekaway/version`
